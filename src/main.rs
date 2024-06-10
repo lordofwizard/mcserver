9a08dea367c6fc6b14ad9d3fe6bf2d2503663c76
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-06-10 12:27:06 +0530

    Added function to download openjdk version, provided a u8 version as argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod config;
+mod utils;
 use crate::config::*;
-
+use crate::utils::download_jdk;
 
 fn main() {
     let config: Config = Config::new();
-
+    download_jdk(8);
     println!("{:?}",config);
 }
