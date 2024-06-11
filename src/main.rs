6f06687847a6943ff1561ee3dd88661b70954ec4
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-06-11 11:49:37 +0530

    Added functionality to the run function which will also parse arguments

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,11 @@ mod run;
 use crate::config::*;
 use crate::utils::*;
 use crate::run::Run;
-use crate::args::MCServer;
-use crate::args::Commands;
-use clap::Parser;
 
 fn main() {
     let config: Config = Config::new();
-    let run = Run::new();
+    let mut run = Run::new();
     //    download_jdk(22);
     //    make_file_tree("homiecraft");
-    MCServer::parse();
     println!("{:?}", config);
 }
