61424722fdd458ecf99355b61d9b68905ae282ea
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-10-16 21:35:07 +0530

    Added simple test suite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main(){
-    println!("New Begining");
+fn main() {
+    println!("Hello, world!");
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
 }
