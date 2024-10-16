2114a9a4c10e1e7706a3005ab2019300c0937eba
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-10-16 22:43:55 +0530

    Added new files with their impls and a basic test that checks few things.

---
+++ src/args.rs
@@ -0,0 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = "MCServer",
+    about = "MCServer a cli utility created to host a Fully Customizable Minecraft Server",
+    author = "LordOfWizard",
+    version = "0.0.1"
+)]
+pub struct MCServer {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Generates a new Minecraft Server instance.
+    Setup,
+    /// Builds a new Minecraft Server Instance.
+    Build,
+    /// Starts the Minecraft Server.
+    Start,
+    /// Stops the Minecraft Server.
+    Stop,
+    /// Prints the logs of the latest run.
+    Log,
+    /// Gives debug information on Currently running server.
+    Check,
+    /// Prints the connectable url of the Minecraft Server.
+    GetUrl,
+}
---
+++ src/setup.rs
@@ -0,0 +1,3 @@
+pub fn setup(){
+    // TODO Build this
+}
