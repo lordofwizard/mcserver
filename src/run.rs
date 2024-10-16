9e3c2bff3d6048aa559b24034ae35bbba0a4b0e6
lordofwizard <advaitpandharpurkar619@gmail.com>
2024-10-16 22:58:12 +0530

    Added relavent subcommand files without any code inside them

--- src/run.rs
+++ src/run.rs
@@ -4,7 +4,10 @@ use crate::args::{MCServer,Commands};
 use crate::build::build;
 use crate::setup::setup;
 use crate::start::start_server;
-
+use crate::stop::stop;
+use crate::log::log;
+use crate::check::check;
+use crate::get_url::get_url;
 
 impl Run {
     pub fn new() {
@@ -17,7 +20,10 @@ impl Run {
             Commands::Setup => setup(),
             Commands::Build => build(),
             Commands::Start => start_server(),
-
+            Commands::Log   => log(),
+            Commands::Check => check(),
+            Commands::Stop  => stop(),
+            Commands::GetUrl => get_url(),
             _ => println!("No Implementation found for this command"),
         }
 
@@ -30,6 +36,10 @@ mod tests {
 
     #[test]
     fn run_has_std_practices() {
+        // Commented this test cause while parsing it fails 
+        // Waits for a user subcommand and takes directly
+        // To help 
+
         //Run::new();
         assert!(true);
     }
